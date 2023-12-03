b72972599a094c4370bf7f71ecb0e031bcdb7ea0
Willem-Jan <wjzijderveld@protonmail.com>
2023-12-03 13:54:56 +0100

    Day 3 part 1 complete

--- year2023/day3/src/main.rs
+++ year2023/day3/src/main.rs
@@ -12,6 +12,7 @@ impl Loc {
     }
 }
 
+#[derive(Debug, Clone)]
 struct Number {
     number: Vec<char>,
     started_at: Loc,
@@ -28,15 +29,60 @@ impl Number {
     fn add_char(&mut self, add: char) {
         self.number.push(add);
     }
+
+    fn get_number(&self) -> usize {
+        self.number
+            .iter()
+            .cloned()
+            .collect::<String>()
+            .parse()
+            .unwrap()
+    }
+
+    fn is_adjacent_to_symbol(&self, schematic: Vec<Vec<char>>) -> bool {
+        let mut start_y = 0;
+        let mut start_x = 0;
+        if self.started_at.y > 0 {
+            // not in top row
+            start_y = self.started_at.y - 1;
+        }
+        if self.started_at.x > 0 {
+            start_x = self.started_at.x - 1;
+        }
+
+        let mut end_x = self.started_at.x + self.number.len();
+        let mut end_y = self.started_at.y + 1;
+        if end_x >= schematic[0].len() - 1 {
+            end_x = schematic[0].len() - 1;
+        }
+        if end_y >= schematic.len() - 1 {
+            end_y = schematic.len() - 1;
+        }
+
+        for x in start_x..end_x + 1 {
+            for y in start_y..end_y + 1 {
+                match schematic[y][x] {
+                    '0'..='9' | '.' => {}, // ignore numbers and dots
+                    _ => return true,
+                }
+            }
+        }
+
+        false
+    }
 }
 
+// wrong answers:
+// - 567240 => too high
+// - 446820 => too low
+//
+// third and correct guess:
+// - 556057
 fn main() {
     let i = input::get_input_lines();
-    println!("Input: {}", i);
 
     let mut schematic: Vec<Vec<char>> = vec![];
-    let mut numbers_found_at: Vec<Loc> = vec![];
-    // let mut symbols_found_at: Vec<Loc> = vec![];
+    let mut found_numbers: Vec<Number> = vec![];
     for (i, line) in i.lines().enumerate() {
         schematic.push(vec![]);
         let mut peekable_chars = line.chars().peekable();
@@ -49,14 +95,13 @@ fn main() {
                     schematic[i].push(c);
 
                     // collect all digits of the number
-                    if c.is_numeric() {
+                    if ('0'..='9').contains(&c) {
                         match current_number {
                             Some(ref mut cn) => cn.add_char(c),
                             None => {
-                                let loc = Loc::new(i, j);
+                                let loc = Loc::new(j, i);
                                 let num = Number::new(loc, c);
                                 current_number = Some(num);
-                                numbers_found_at.push(loc)
                             }
                         }
                     }
@@ -66,15 +111,30 @@ fn main() {
                     // peeking here to avoid dealing with EOL/EOF fun outside the loops
                     let peek = peekable_chars.peek();
                     match peek {
-                        Some(x) if x.is_numeric() => continue, // number is not finished, continue
-                                                               // to next iteration
-                        Some(_) | None => current_number = None,   // dot or symbol
-                    }
-                },
+                        Some(x) if ('0'..='9').contains(x) => continue, // number is not finished, continue
+                        // to next iteration
+                        Some(_) | None => {
+                            // dot or symbol, collect buffered number if there is one
+                            match current_number {
+                                Some(ref mut cn) => found_numbers.push(cn.clone()),
+                                None => {}
+                            }
+                            // reset number and start looking for the next one
+                            current_number = None;
+                        }
+                    };
+                }
                 None => break, // no next character, break loop
             }
         }
     }
 
-    println!("Numbers found: {:?}", numbers_found_at);
+    let mut total = 0;
+    for number in found_numbers {
+        if number.is_adjacent_to_symbol(schematic.clone()) {
+            total += number.get_number();
+        }
+    }
+
+    println!("Sum of numbers adjecent to symbol: {}", total);
 }
