6ea4ddbe3e1d9ff82467ad0df0c2135947f18925
Willem-Jan <wjzijderveld@protonmail.com>
2023-12-05 08:39:37 +0100

    day 4 - part 2

--- year2023/day4/src/main.rs
+++ year2023/day4/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, hash_map::RandomState};
+use std::collections::{HashSet, BTreeMap, HashMap, hash_map::RandomState};
 
 use input;
 
@@ -15,25 +15,25 @@ impl Game {
         Self { id, guessed_numbers, winning_numbers }
     }
 
-    fn get_score(&self) -> usize {
+    fn get_count(&self) -> usize {
         let guessed = self.guessed_numbers.clone();
         let winning = self.winning_numbers.clone();
 
         let b: HashSet<usize, RandomState> = HashSet::from_iter(winning);
-        let count = HashSet::from_iter(guessed).intersection(&b).count();
-        let score = if count > 1 { 2_usize.pow((count-1).try_into().unwrap()) } else { count };
-        if std::env::var("DEBUG").is_ok() {
-            println!("Score game {} => {}: {}", self.id, count, score);
-        }
-
-        score
+        HashSet::from_iter(guessed).intersection(&b).count()
     }
 }
 
+// Wrong guess:
+// - 2794230 - too low
+//
+// Correct guess:
+// - 5625994
 fn main() {
     let i = input::get_input_lines();
 
-    let mut total = 0;
+    let mut card_copies = HashMap::new();
+    let mut games = BTreeMap::new();
     for line in i.lines() {
         let game = match line.split(": ").collect::<Vec<&str>>().as_slice() {
             [game_description, cards] => {
@@ -58,7 +58,19 @@ fn main() {
             },
             _ => panic!("invalid game")
         };
-        total += game.get_score();
+        card_copies.insert(game.id, 1);
+        games.insert(game.id, game);
     }
-    println!("Total: {}", total);
+
+    for (id, game) in games {
+        let count = game.get_count();
+        // println!("game={} count={} copies={:?}", id, count, card_copies.clone());
+        for next_id in id+1..=(id+count) {
+            let immutable_card_copies = card_copies.clone();
+            card_copies.entry(next_id).and_modify(|x: &mut i32| *x += immutable_card_copies.get(&id).unwrap_or(&0));
+        }
+    }
+    let total_cards: i32 = card_copies.into_values().sum();
+
+    println!("Total: {}", total_cards);
 }
